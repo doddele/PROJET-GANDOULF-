190a06788fa70e135915718c85a79321542b4050
Jeffuuuu <120730443+Jeffuuuu@users.noreply.github.com>
2022-12-24 00:02:38 +0100

    Add files via upload

---
+++ Monstres.java
@@ -0,0 +1,28 @@
+public class Monstres {
+            public String nomMonstre;
+            public int PVMonstre;
+            public int Attaque;
+            public int Defense;
+            public int Niveau;
+            public int XPdonne;
+            public int PV;
+            public int Esquive;
+            public int Vitesse;
+
+
+            public Monstres ( String nomMonstre,int PVMonstre, int Attaque, int Defense, int Niveau, int XPdonne, int PV, int Esquive, int Vitesse){
+                this.nomMonstre = nomMonstre;
+                this.PVMonstre = PVMonstre;
+                this.Attaque = Attaque;
+                this.Defense = Defense;
+                this.Niveau = Niveau;
+                this.XPdonne = XPdonne;
+                this.PV = PV;
+                this.Esquive = Esquive;
+                this.Vitesse = Vitesse;
+
+            }
+
+
+            }
+
