859065ca92beedb9e2a358bfef9ce8c5ba1505b1
doddele <108028247+doddele@users.noreply.github.com>
2023-01-23 00:01:14 +0100

    Delete Marchand.java

--- Marchand.java
+++
@@ -1,8 +0,0 @@
-public interface Marchand {
-    public void acheterPotions(Personnage joueur);
-    public void vendrePotions();
-    public void acheterEquipement(Personnage joueur);
-    public void vendreEquipement();
-    public void acheterArmure(Personnage joueur);
-    public void acheterArme(Personnage joueur);
-}
